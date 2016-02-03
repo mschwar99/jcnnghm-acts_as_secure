783219d6d136c6672610868dee9a839e847e75d8
Matt Schwartz <matt@matthewschwartz.me>
2016-02-03 12:31:35 -0600

    patch ActiveRecord::Acts::ActsAsSecure#decrypt_secure_columns for ActiveRecord 4.2

--- lib/acts_as_secure/acts_as_secure.rb
+++ lib/acts_as_secure/acts_as_secure.rb
@@ -1,31 +1,31 @@
-module ActiveRecord; module Acts; end; end 
+module ActiveRecord; module Acts; end; end
 
 module ActiveRecord::Acts::ActsAsSecure
-  
+
   require 'yaml'
-    
+
   def self.included(base)
     base.extend(ClassMethods)
   end
-  
+
   module ClassMethods
-            
+
     def acts_as_secure(options = {})
       parse_options!(options)
       add_callbacks
       extend(ActsAsSecureClassMethods)
       send(:include, InstanceMethods)
     end
-      
+
   private
-            
+
     def parse_options!(options)
       @secure_except = unsecure_columns(options.delete(:except))
       @secure_storage_type = options.delete(:storage_type) || :binary
       @secure_crypto_provider = options.delete(:crypto_provider)
       fail("Unknown option(s): #{ options.keys.join(', ') }") unless options.empty?
     end
-    
+
     def add_callbacks
       before_save :encrypt_secure_columns
       after_save :decrypt_secure_columns
@@ -35,9 +35,9 @@ module ActiveRecord::Acts::ActsAsSecure
     def unsecure_columns(*names)
       names.flatten.collect(&:to_s)
     end
-        
+
     module ActsAsSecureClassMethods
-      
+
       def inherited(sub)
 
         [:secure_except, :secure_storage_type, :secure_crypto_provider].each do |p|
@@ -47,7 +47,7 @@ module ActiveRecord::Acts::ActsAsSecure
         super
 
       end
-      
+
       def with_crypto_provider(provider)
         begin
           original_provider = @secure_crypto_provider
@@ -65,10 +65,9 @@ module ActiveRecord::Acts::ActsAsSecure
       def secure_crypto_provider
         @secure_crypto_provider
       end
-      
+
     end
-    
-    
+
     module InstanceMethods
 
       def encrypt_secure_columns
@@ -76,12 +75,17 @@ module ActiveRecord::Acts::ActsAsSecure
           self[col.name] = secure_encrypt(self[col.name])
         end
       end
-      
+
       def decrypt_secure_columns
         @encrypted_attributes = {}
         self.class.secure_columns.each do |col|
-          @encrypted_attributes[col.name] = send("#{ col.name }_before_type_cast")
-          self[col.name] = secure_decrypt(send("#{ col.name }_before_type_cast")) unless self[col.name].nil?
+          if ActiveRecord::VERSION::MAJOR == 4 && ActiveRecord::VERSION::MINOR >= 2
+            before_type_cast = send("#{ col.name }")#read_attribute_before_type_cast(col.name)
+          else
+            before_type_cast = send("#{ col.name }_before_type_cast")
+          end
+          @encrypted_attributes[col.name] = before_type_cast
+          self[col.name] = secure_decrypt(before_type_cast) unless self[col.name].nil?
         end
       end
 
@@ -92,13 +96,13 @@ module ActiveRecord::Acts::ActsAsSecure
           @encrypted_attributes[attr_name.to_s]
         end
       end
-      
+
     private
-      
+
       def secure_encrypt(arg)
         secure_crypto_provider.encrypt(arg.to_yaml)
-      end 
-           
+      end
+
       def secure_decrypt(arg)
         begin
           YAML.load(secure_crypto_provider.decrypt(arg))
@@ -106,15 +110,15 @@ module ActiveRecord::Acts::ActsAsSecure
           raise "Failed to decode the field. Incorrect key?"
         end
       end
-      
+
       def secure_crypto_provider
         self.class.secure_crypto_provider || fail('No crypto provider defined')
       end
-      
+
     end
-    
+
   end
-  
+
 end
 
 ActiveRecord::Base.send(:include, ActiveRecord::Acts::ActsAsSecure)
